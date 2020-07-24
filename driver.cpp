baee295e876708db0e658596dc3cf94206c9cdfb
U-DESKTOP-5IRJRT6\brian <brianalas01@gmail.com>
2020-07-24 16:24:09 -0400

    Added description to the driver

--- driver.cpp
+++ driver.cpp
@@ -1,3 +1,9 @@
+//Author: Balas47
+//Description: The purpose of this file is for testing the functionality of the
+//             individual classes, as well as the game of the whole. When
+//             finished, the game will be started and controlled here.
+
+
 #include<utility>
 #include<iostream>
 #include"playerClass.h"
